4a4d693c0d70fefde65cbf1958ae9e1ccd814c7b
drixxu <113541819+drixxu@users.noreply.github.com>
2023-03-28 00:03:22 +0800

    adada

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand>CPEeeeeeeeeeee</Navbar.Brand>
+                        <Navbar.Brand>CPE</Navbar.Brand>
                     </LinkContainer>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
